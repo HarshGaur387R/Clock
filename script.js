60d2fc4303e18659f04df483cd0765f86bb90f73
HarshGa387R <hgaur701@gmail.com>
2022-11-25 15:58:20 +0530

    nothing speacial

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function printDate(){
     let seconds = d.getSeconds();
     let minutes = d.getMinutes();
     let hours = d.getHours();
-
+    
     hourArrow.style.transform = `rotate(${30*hours + minutes/2}deg)`;
     minuteArrow.style.transform = `rotate(${6*minutes}deg)`;
     secondArrow.style.transform = `rotate(${6*seconds}deg)`;
